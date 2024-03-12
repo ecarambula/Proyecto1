df913bdf0155e75212d2f50ac02d115f9904d417
Ernesto Rodríguez Carámbula <ecarambula@gmail.com>
2024-03-12 14:47:03 +0000

    12-03

---
+++ dashboard/app.py
@@ -0,0 +1,101 @@
+# Importar bibliotecas
+from shiny import App, render, ui, reactive
+import pandas as pd
+import plotnine as p9
+
+# Importar datos
+datos = pd.read_csv(
+    filepath_or_buffer="datos_tratados.csv",
+    converters={"Date": pd.to_datetime}
+    )
+
+# Interfase de usuario
+app_ui = ui.page_navbar(
+    ui.nav(
+        "",
+        ui.layout_sidebar(
+            ui.panel_sidebar(
+                
+                ui.markdown(
+                "Dashboard analítica para diagnosticar o comportamento" +
+                "histórico da inflação brasileira, medida pelos principais" +
+                "indicadores de conjuntura econômica. Utilize as opções" +
+                "abaixo para customização da análise."
+                ),
+
+                ui.input_select(
+                    id = "indicador",
+                    label = ui.strong("Indicador:"),
+                    choices = datos.columns[1:].tolist(),
+                    selected = "IPCA",
+                    multiple = False    
+                ),
+
+                ui.input_date_range(
+                    id = "fechas",
+                    label = ui.strong("Fecha inicial y final:"),
+                    start = datos.Date.astype(str).min(),
+                    end = datos.Date.astype(str).max(),
+                    min = datos.Date.astype(str).min(),
+                    max = datos.Date.astype(str).max(),
+                    format = "mm/yyyy",
+                    startview = "year",
+                    language = "es",
+                    separator = " - "
+                ),
+
+                ui.input_numeric(
+                    id = "anio",
+                    label = "Comparador de años:",
+                    value = int(datos.Date.dt.year.max()),
+                    min = int(datos.Date.dt.year.min()),
+                    max = int(datos.Date.dt.year.max()),
+                    step = 1
+                ),
+
+                ui.input_checkbox_group(
+                    id = "componentes",
+                    label = ui.strong("Componentes:"),
+                    choices = ["% a.m.", "Tendência", "Sazonalidade", "Média"],
+                    selected = ["% a.m."]
+                ),
+
+                ui.markdown(
+                    """
+                    Fuente: FGV e IBGE
+                    Elaborado por EVRC
+                    """
+                ),
+                width = 3
+            ),
+            
+            ui.panel_main(
+                ui.row(ui.output_plot("grafico_estacionalidad")),
+                ui.row(ui.output_plot("grafico_componentes"))
+            )
+        )
+    ),
+    title = ui.strong("Diagnóstico de la inflación en Brasil"),
+    bg = "blue",
+    inverse = True
+)
+
+# Servidor
+def server(input, output, session):
+
+    @reactive.Calc
+    def prepara_componentes():
+        
+    
+    @output
+    @render.plot
+    def grafico_componentes():
+        grafico = (
+            p9.ggplot(datos) +
+            p9.aes(x = "Date", y = "IPCA") +
+            p9.geom_line()
+        )
+        return grafico
+    
+#Dashboard Shiny 
+app = App(app_ui, server)
